61f82f02d9daa615978ff133217e7bae487a751f
RandallBalestriero <RandallBalestriero@users.noreply.github.com>
2025-11-09 05:06:35 +0000

    deploy: 2b267be5d5767919df410d5f34e945c6041eeed2

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.1.dev1+g6566e379d.d20251109',
+    VERSION: '0.1.dev1+g2b267be5d.d20251109',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'dirhtml',
